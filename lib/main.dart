910b4b21e72cd34b81757cc718c07f60a98decca
Chathuranga97lj <chathuit97@gmail.com>
2023-02-28 12:12:20 +0530

    set add new contact wihtout refresh hompage

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Sample App"),
-        ),
-      ),
-    );
-  }
+  runApp(MaterialApp(
+    title: "flutter Demo",
+    theme: ThemeData(
+      primaryColor: Colors.blue,
+    ),
+    debugShowCheckedModeBanner: false,
+    home: const HomePage(),
+    routes: {'/new-contact': (context) => const NewContactView()},
+  ));
 }
 
 // class for handle contact
@@ -35,7 +23,8 @@ class Contact {
 // contact book functions class
 class ContactBook {
   ContactBook._sharedInstance();
-  static final ContactBook _shared = ContactBook._sharedInstance(); // singleton instance
+  static final ContactBook _shared =
+      ContactBook._sharedInstance(); // singleton instance
   // factory constructor return already have instance, not create new instance
   factory ContactBook() => _shared;
 
@@ -50,10 +39,83 @@ class ContactBook {
   void remove({required Contact contact}) {
     _contacts.remove(contact);
   }
-   // retrieve contacts with index
+
+  // retrieve contacts with index
   Contact? contact({required int atIndex}) =>
       _contacts.length > atIndex ? _contacts[atIndex] : null;
-
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    // use single instance
+    final contactBook = ContactBook();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Home Page"),
+      ),
+      body: ListView.builder(
+        itemCount: contactBook.length,
+        itemBuilder: (context, index) {
+          final contact = contactBook.contact(atIndex: index)!;
+          return ListTile(
+            title: Text(contact.name),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.of(context).pushNamed('/new-contact');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+// add new contact to contact book
+class NewContactView extends StatefulWidget {
+  const NewContactView({Key? key}) : super(key: key);
+
+  @override
+  State<NewContactView> createState() => _NewContactViewState();
+}
+
+class _NewContactViewState extends State<NewContactView> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Add a new contact"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+                hintText: 'Enter a new contact name here'),
+          ),
+          TextButton(
+              onPressed: () {
+                final contact = Contact(name: _controller.text);
+                ContactBook().add(contact: contact);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Add contact'))
+        ],
+      ),
+    );
+  }
+}
